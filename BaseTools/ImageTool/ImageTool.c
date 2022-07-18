ff98d7a74e30f7a24df1e2f1a5763aca3b65bd6d
Mikhail Krichanov <krichanov@ispras.ru>
2022-07-18 11:40:27 +0300

    ImageTool: Refactored ImageTool interface.

--- BaseTools/ImageTool/ImageTool.c
+++ BaseTools/ImageTool/ImageTool.c
@@ -1,25 +1,21 @@
 #include "ImageTool.h"
 
-int main (int argc, char *argv[])
+EFI_STATUS
+PeToUe (
+	IN const char *PeName,
+  IN const char *UeName
+  )
 {
   void       *Pe;
   uint32_t   PeSize;
   void       *Ue;
   uint32_t   UeSize;
   bool       Result;
-  EFI_STATUS Status;
   image_tool_image_info_t Image;
 
-  Status = ElfToPe (argv[1], "Pe.efi");
-  if (EFI_ERROR (Status)) {
-    raise();
-    return -1;
-  }
-
-  Pe = UserReadFile ("Pe.efi", &PeSize);
+  Pe = UserReadFile (PeName, &PeSize);
   if (Pe == NULL) {
-    raise();
-    return -1;
+    return EFI_ABORTED;
   }
 
   Result = ToolContextConstructPe(&Image, Pe, PeSize);
@@ -28,30 +24,26 @@ int main (int argc, char *argv[])
   Pe = NULL;
 
   if (!Result) {
-    raise();
-    return -1;
+    return EFI_ABORTED;
   }
 
   Result = CheckToolImage(&Image);
   if (!Result) {
     ToolImageDestruct(&Image);
-    raise();
-    return -1;
+    return EFI_ABORTED;
   }
 
   Result = ImageConvertToXip(&Image);
   if (!Result) {
     ToolImageDestruct(&Image);
-    raise();
-    return -1;
+    return EFI_ABORTED;
   }
 
   Ue = ToolImageEmitUe(&Image, &UeSize);
 
   if (Ue == NULL) {
     ToolImageDestruct(&Image);
-    raise();
-    return -1;
+    return EFI_ABORTED;
   }
 
   ToolImageDestruct(&Image);
@@ -60,10 +52,31 @@ int main (int argc, char *argv[])
   EFI_STATUS Status = UeInitializeContext(&UeContext, Ue, UeSize);
   printf("UE status - %llu\n", Status);*/
 
-  UserWriteFile(argv[2], Ue, UeSize);
+  UserWriteFile(UeName, Ue, UeSize);
 
   free(Ue);
   Ue = NULL;
+
+  return EFI_SUCCESS;
+}
+
+int main (int argc, char *argv[])
+{
+  EFI_STATUS Status;
+
+  if (strcmp (argv[4], "ElfToPe") == 0) {
+    Status = ElfToPe (argv[1], argv[2], argv [3]);
+    if (EFI_ERROR (Status)) {
+      raise();
+      return -1;
+    }
+  } else if (strcmp (argv[4], "PeToUe") == 0) {
+    Status = PeToUe (argv[1], argv[2]);
+    if (EFI_ERROR (Status)) {
+      raise();
+      return -1;
+    }
+  }
 
   return 0;
 }
