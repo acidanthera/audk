5f1f3b82dd445fa981fef87a435ab8911107b4ca
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2022-12-07 11:59:05 +0300

    MdePkg/X64/ProcessorBind.h: Fixes CLANGDWARF X64 compilation
    Corrects typedef redefinition, in particular for openssl e_os2.h
    
    Signed-off-by: Savva Mitrofanov <savvamtr@gmail.com>

--- MdePkg/Include/X64/ProcessorBind.h
+++ MdePkg/Include/X64/ProcessorBind.h
@@ -169,6 +169,53 @@ typedef char CHAR8;
 /// 1-byte signed value
 ///
 typedef signed char INT8;
+#elif defined (__clang__)
+///
+/// 8-byte unsigned value
+///
+typedef __UINT64_TYPE__ UINT64;
+///
+/// 8-byte signed value
+///
+typedef __INT64_TYPE__ INT64;
+///
+/// 4-byte unsigned value
+///
+typedef unsigned int UINT32;
+///
+/// 4-byte signed value
+///
+typedef signed int INT32;
+///
+/// 2-byte unsigned value
+///
+typedef unsigned short UINT16;
+///
+/// 2-byte Character.  Unless otherwise specified all strings are stored in the
+/// UTF-16 encoding format as defined by Unicode 2.1 and ISO/IEC 10646 standards.
+///
+typedef unsigned short CHAR16;
+///
+/// 2-byte signed value
+///
+typedef short INT16;
+///
+/// Logical Boolean.  1-byte value containing 0 for FALSE or a 1 for TRUE.  Other
+/// values are undefined.
+///
+typedef unsigned char BOOLEAN;
+///
+/// 1-byte unsigned value
+///
+typedef unsigned char UINT8;
+///
+/// 1-byte Character
+///
+typedef char CHAR8;
+///
+/// 1-byte signed value
+///
+typedef signed char INT8;
 #else
 ///
 /// 8-byte unsigned value
