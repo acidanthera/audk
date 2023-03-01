7be0d61af45c3c67aecd4162d4e475c8ce9350f2
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2023-03-01 12:51:09 +0300

    MdePkg: Moved DEBUG_RAISE() to DebugLib.h.

--- MdePkg/Include/Base.h
+++ MdePkg/Include/Base.h
@@ -885,21 +885,6 @@ STATIC_ASSERT (ALIGNOF (__VERIFY_UINT32_ENUM_SIZE) == sizeof (__VERIFY_UINT32_EN
 **/
 #define BASE_CR(Record, TYPE, Field)  ((TYPE *) ((CHAR8 *) (Record) - OFFSET_OF (TYPE, Field)))
 
-#define DEBUG_RAISE()                                                                                         \
-    do {                                                                                                      \
-      if ((PcdGet8 (PcdDebugRaisePropertyMask) & DEBUG_PROPERTY_DEBUG_PRINT_ENABLED) != 0) {                  \
-        DEBUG ((DEBUG_WARN, "DEBUG RAISE: Constraint violation in %a:%a:%u\n", __FILE__, __func__, __LINE__));\
-      }                                                                                                       \
-                                                                                                              \
-      if ((PcdGet8 (PcdDebugRaisePropertyMask) & DEBUG_PROPERTY_DEBUG_ASSERT_ENABLED) != 0) {                 \
-        ASSERT (FALSE);                                                                                       \
-      }                                                                                                       \
-                                                                                                              \
-      if ((PcdGet8 (PcdDebugRaisePropertyMask) & DEBUG_PROPERTY_ASSERT_BREAKPOINT_ENABLED) != 0) {            \
-        CpuBreakpoint ();                                                                                     \
-      }                                                                                                       \
-    } while (FALSE)
-
 /**
   Checks whether a value is a power of two.
 
