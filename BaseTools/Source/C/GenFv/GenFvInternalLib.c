df6f57935b95eba839e2203bc5c38c9a3ba2f242
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2023-03-17 22:45:33 +0100

    BaseTools/GenFvInternalLib: Remove unused BaseOfCode parameter

--- BaseTools/Source/C/GenFv/GenFvInternalLib.c
+++ BaseTools/Source/C/GenFv/GenFvInternalLib.c
@@ -1590,7 +1590,6 @@ Returns:
   EFI_STATUS                Status;
   EFI_FILE_SECTION_POINTER  Pe32Section;
   UINT32                    EntryPoint;
-  UINT32                    BaseOfCode;
   UINT16                    MachineType;
   EFI_PHYSICAL_ADDRESS      PeiCorePhysicalAddress;
   EFI_PHYSICAL_ADDRESS      SecCorePhysicalAddress;
@@ -1669,7 +1668,6 @@ Returns:
   Status        = GetPe32Info (
                     (VOID *)((UINTN)Pe32Section.Pe32Section + SecHeaderSize),
                     &EntryPoint,
-                    &BaseOfCode,
                     &MachineType
                     );
 
@@ -1723,7 +1721,6 @@ Returns:
     Status        = GetPe32Info (
                       (VOID *)((UINTN)Pe32Section.Pe32Section + SecHeaderSize),
                       &EntryPoint,
-                      &BaseOfCode,
                       &MachineType
                       );
 
@@ -1960,7 +1957,6 @@ Returns:
 {
   EFI_STATUS  Status;
   UINT32      EntryPoint;
-  UINT32      BaseOfCode;
 
   if (CoreMachineType == NULL) {
     return EFI_INVALID_PARAMETER;
@@ -1969,7 +1965,6 @@ Returns:
   Status = GetPe32Info (
              (VOID *)((UINTN)Pe32Section.Pe32Section + GetSectionHeaderLength (Pe32Section.CommonHeader)),
              &EntryPoint,
-             &BaseOfCode,
              CoreMachineType
              );
   if (EFI_ERROR (Status)) {
@@ -2011,7 +2006,6 @@ Returns:
 {
   EFI_STATUS            Status;
   UINT32                EntryPoint;
-  UINT32                BaseOfCode;
   UINT16                MachineType;
   EFI_PHYSICAL_ADDRESS  EntryPhysicalAddress;
 
@@ -2022,7 +2016,6 @@ Returns:
   Status = GetPe32Info (
              (VOID *)((UINTN)Pe32Section.Pe32Section + GetSectionHeaderLength (Pe32Section.CommonHeader)),
              &EntryPoint,
-             &BaseOfCode,
              &MachineType
              );
   if (EFI_ERROR (Status)) {
@@ -2396,7 +2389,6 @@ EFI_STATUS
 GetPe32Info (
   IN UINT8    *Pe32,
   OUT UINT32  *EntryPoint,
-  OUT UINT32  *BaseOfCode,
   OUT UINT16  *MachineType
   )
 
@@ -2412,7 +2404,6 @@ Arguments:
 
   Pe32          Beginning of the PE32.
   EntryPoint    Offset from the beginning of the PE32 to the image entry point.
-  BaseOfCode    Base address of code.
   MachineType   Magic number for the machine type.
 
 Returns:
@@ -2444,7 +2435,6 @@ Returns:
     // By TeImage Header to get output
     //
     *EntryPoint  = TeHeader->AddressOfEntryPoint + sizeof (EFI_TE_IMAGE_HEADER) - TeHeader->StrippedSize;
-    *BaseOfCode  = TeHeader->BaseOfCode + sizeof (EFI_TE_IMAGE_HEADER) - TeHeader->StrippedSize;
     *MachineType = TeHeader->Machine;
   } else {
     //
@@ -2478,7 +2468,6 @@ Returns:
     // Get output
     //
     *EntryPoint  = ImgHdr->Pe32.OptionalHeader.AddressOfEntryPoint;
-    *BaseOfCode  = ImgHdr->Pe32.OptionalHeader.BaseOfCode;
     *MachineType = ImgHdr->Pe32.FileHeader.Machine;
   }
 
