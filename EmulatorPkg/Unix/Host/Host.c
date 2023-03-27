07dc63876c2b53880485e3d25ca02b83952ec326
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2023-03-28 00:18:57 +0200

    EmulatorPkg/Host: Remove orphaned SecImageRead()

--- EmulatorPkg/Unix/Host/Host.c
+++ EmulatorPkg/Unix/Host/Host.c
@@ -836,45 +836,6 @@ CountSeparatorsInString (
   return Count;
 }
 
-EFI_STATUS
-EFIAPI
-SecImageRead (
-  IN     VOID   *FileHandle,
-  IN     UINTN  FileOffset,
-  IN OUT UINTN  *ReadSize,
-  OUT    VOID   *Buffer
-  )
-
-/*++
-
-Routine Description:
-  Support routine for the PE/COFF Loader that reads a buffer from a PE/COFF file
-
-Arguments:
-  FileHandle - The handle to the PE/COFF file
-  FileOffset - The offset, in bytes, into the file to read
-  ReadSize   - The number of bytes to read from the file starting at FileOffset
-  Buffer     - A pointer to the buffer to read the data into.
-
-Returns:
-  EFI_SUCCESS - ReadSize bytes of data were read into Buffer from the PE/COFF file starting at FileOffset
-
-**/
-{
-  CHAR8  *Destination8;
-  CHAR8  *Source8;
-  UINTN  Length;
-
-  Destination8 = Buffer;
-  Source8      = (CHAR8 *)((UINTN)FileHandle + FileOffset);
-  Length       = *ReadSize;
-  while (Length--) {
-    *(Destination8++) = *(Source8++);
-  }
-
-  return EFI_SUCCESS;
-}
-
 BOOLEAN
 IsPdbFile (
   IN  CONST CHAR8   *PdbFileName
