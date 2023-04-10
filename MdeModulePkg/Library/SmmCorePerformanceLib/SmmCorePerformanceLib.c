9c43408450726d02d0cc5b0141fc0aba146cfc00
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2023-04-10 10:49:29 +0200

    MdeModulePkg/ExtendedFirmwarePerformance: Fix FPDT struct alignment

--- MdeModulePkg/Library/SmmCorePerformanceLib/SmmCorePerformanceLib.c
+++ MdeModulePkg/Library/SmmCorePerformanceLib/SmmCorePerformanceLib.c
@@ -619,6 +619,7 @@ InsertFpdtRecord (
         FpdtRecordPtr.GuidEvent->Header.Length   = sizeof (FPDT_GUID_EVENT_RECORD);
         FpdtRecordPtr.GuidEvent->Header.Revision = FPDT_RECORD_REVISION_1;
         FpdtRecordPtr.GuidEvent->ProgressID      = PerfId;
+        FpdtRecordPtr.GuidEvent->Reserved        = 0;
         FpdtRecordPtr.GuidEvent->Timestamp       = TimeStamp;
         CopyMem (&FpdtRecordPtr.GuidEvent->Guid, &ModuleGuid, sizeof (FpdtRecordPtr.GuidEvent->Guid));
         if ((CallerIdentifier == NULL) && (PerfId == MODULE_END_ID) && (mCachedLength != 0)) {
@@ -649,6 +650,7 @@ InsertFpdtRecord (
         FpdtRecordPtr.GuidQwordEvent->Header.Length   = sizeof (FPDT_GUID_QWORD_EVENT_RECORD);
         FpdtRecordPtr.GuidQwordEvent->Header.Revision = FPDT_RECORD_REVISION_1;
         FpdtRecordPtr.GuidQwordEvent->ProgressID      = PerfId;
+        FpdtRecordPtr.GuidQwordEvent->Reserved        = 0;
         FpdtRecordPtr.GuidQwordEvent->Timestamp       = TimeStamp;
         FpdtRecordPtr.GuidQwordEvent->Qword           = mLoadImageCount;
         CopyMem (&FpdtRecordPtr.GuidQwordEvent->Guid, &ModuleGuid, sizeof (FpdtRecordPtr.GuidQwordEvent->Guid));
@@ -679,6 +681,7 @@ InsertFpdtRecord (
         FpdtRecordPtr.DualGuidStringEvent->Header.Length   = sizeof (FPDT_DUAL_GUID_STRING_EVENT_RECORD);
         FpdtRecordPtr.DualGuidStringEvent->Header.Revision = FPDT_RECORD_REVISION_1;
         FpdtRecordPtr.DualGuidStringEvent->ProgressID      = PerfId;
+        FpdtRecordPtr.DualGuidStringEvent->Reserved        = 0;
         FpdtRecordPtr.DualGuidStringEvent->Timestamp       = TimeStamp;
         CopyMem (&FpdtRecordPtr.DualGuidStringEvent->Guid1, CallerIdentifier, sizeof (FpdtRecordPtr.DualGuidStringEvent->Guid1));
         CopyMem (&FpdtRecordPtr.DualGuidStringEvent->Guid2, Guid, sizeof (FpdtRecordPtr.DualGuidStringEvent->Guid2));
@@ -710,6 +713,7 @@ InsertFpdtRecord (
         FpdtRecordPtr.DynamicStringEvent->Header.Length   = sizeof (FPDT_DYNAMIC_STRING_EVENT_RECORD);
         FpdtRecordPtr.DynamicStringEvent->Header.Revision = FPDT_RECORD_REVISION_1;
         FpdtRecordPtr.DynamicStringEvent->ProgressID      = PerfId;
+        FpdtRecordPtr.DynamicStringEvent->Reserved        = 0;
         FpdtRecordPtr.DynamicStringEvent->Timestamp       = TimeStamp;
         CopyMem (&FpdtRecordPtr.DynamicStringEvent->Guid, &ModuleGuid, sizeof (FpdtRecordPtr.DynamicStringEvent->Guid));
         CopyStringIntoPerfRecordAndUpdateLength (FpdtRecordPtr.DynamicStringEvent->String, StringPtr, &FpdtRecordPtr.DynamicStringEvent->Header.Length);
@@ -735,6 +739,7 @@ InsertFpdtRecord (
           FpdtRecordPtr.DynamicStringEvent->Header.Length   = sizeof (FPDT_DYNAMIC_STRING_EVENT_RECORD);
           FpdtRecordPtr.DynamicStringEvent->Header.Revision = FPDT_RECORD_REVISION_1;
           FpdtRecordPtr.DynamicStringEvent->ProgressID      = PerfId;
+          FpdtRecordPtr.DynamicStringEvent->Reserved        = 0;
           FpdtRecordPtr.DynamicStringEvent->Timestamp       = TimeStamp;
           CopyMem (&FpdtRecordPtr.DynamicStringEvent->Guid, &ModuleGuid, sizeof (FpdtRecordPtr.DynamicStringEvent->Guid));
           CopyStringIntoPerfRecordAndUpdateLength (FpdtRecordPtr.DynamicStringEvent->String, StringPtr, &FpdtRecordPtr.DynamicStringEvent->Header.Length);
@@ -758,6 +763,7 @@ InsertFpdtRecord (
     FpdtRecordPtr.DynamicStringEvent->Header.Length   = sizeof (FPDT_DYNAMIC_STRING_EVENT_RECORD);
     FpdtRecordPtr.DynamicStringEvent->Header.Revision = FPDT_RECORD_REVISION_1;
     FpdtRecordPtr.DynamicStringEvent->ProgressID      = PerfId;
+    FpdtRecordPtr.DynamicStringEvent->Reserved        = 0;
     FpdtRecordPtr.DynamicStringEvent->Timestamp       = TimeStamp;
     if (Guid != NULL) {
       //
