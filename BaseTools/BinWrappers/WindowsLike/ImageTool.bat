e8e82d235818abd0a6f17d090c9765591cd94b6f
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2023-04-16 16:33:16 +0200

    ImageTool: Unify 32-bit and 64-bit tool variants

--- BaseTools/BinWrappers/WindowsLike/ImageTool.bat
+++ BaseTools/BinWrappers/WindowsLike/ImageTool.bat
@@ -3,28 +3,4 @@
 ::  SPDX-License-Identifier: BSD-3-Clause
 ::
 @echo off
-set args=%2
-:start
-shift /2
-if "%2"=="" goto done
-set args=%args% %2
-goto start
-
-:done
-if "%1%"=="IA32" (
-  call ImageTool32.exe %args%
-  exit
-  )
-if "%1%"=="ARM" (
-  call ImageTool32.exe %args%
-  exit
-  )
-if "%1%"=="X64" (
-  call ImageTool64.exe %args%
-  exit
-)
-if "%1%"=="AARCH64" (
-  call ImageTool64.exe %args%
-  exit
-)
-echo ImageTool for %1% is not supported
+call ImageTool.exe %*
