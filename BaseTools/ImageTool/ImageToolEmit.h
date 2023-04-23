41a8eaf22b0661fe98b917f352838646a6d8fac4
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2023-04-23 21:46:50 +0200

    ImageTool: Separate image generation from file I/O logic

---
+++ BaseTools/ImageTool/ImageToolEmit.h
@@ -0,0 +1,27 @@
+/** @file
+  Copyright (c) 2023, Marvin Häuser. All rights reserved.
+  SPDX-License-Identifier: BSD-3-Clause
+**/
+
+#ifndef IMAGE_TOOL_EMIT_H
+#define IMAGE_TOOL_EMIT_H
+
+#include <stdint.h>
+
+#include <Base.h>
+
+void *
+ToolImageEmit (
+  OUT uint32_t    *OutputFileSize,
+  IN  const void  *Buffer,
+  IN  uint64_t    BufferSize,
+  IN  int8_t      Format,
+  IN  int32_t     Type,
+  IN  void        *HiiFile,
+  IN  uint32_t    HiiFileSize,
+  IN  bool        Relocate,
+  IN  uint64_t    BaseAddress,
+  IN  const char  *SymbolsPath OPTIONAL
+  );
+
+#endif // IMAGE_TOOL_EMIT_H
