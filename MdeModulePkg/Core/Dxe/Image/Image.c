5019c20d35862e2379ef9c0f93f0c97a4cbc2602
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2023-05-05 11:33:30 +0200

    MdeModulePkg/DxeCore: Re-use ImageIsFromFv for memory protection

--- MdeModulePkg/Core/Dxe/Image/Image.c
+++ MdeModulePkg/Core/Dxe/Image/Image.c
@@ -1395,7 +1395,7 @@ CoreLoadImageCommon (
   }
 
   Status = EFI_SUCCESS;
-  ProtectUefiImage (Image, &ImageContext);
+  ProtectUefiImage (&Image->Info, ImageIsFromFv, &ImageContext);
 
   RegisterMemoryProfileImage (
     Image->LoadedImageDevicePath,
