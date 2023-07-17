17776c50eeb14dd4800be40d2f6c3de6dc7b1c9d
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2023-07-17 20:01:08 +0200

    MdePkg/UeImage: Remove support for SAL drivers

--- BaseTools/ImageTool/ImageTool.c
+++ BaseTools/ImageTool/ImageTool.c
@@ -321,11 +321,6 @@ NameToType (
     return EFI_IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER;
   }
 
-  if ((strcmp (TypeName, "DXE_SAL_DRIVER") == 0)
-    || (strcmp (TypeName, "SAL_RT_DRIVER") == 0)) {
-    return EFI_IMAGE_SUBSYSTEM_SAL_RUNTIME_DRIVER;
-  }
-
   return -1;
 }
 
