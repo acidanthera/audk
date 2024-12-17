a5a77d6720ec5f1a377aef2c4d10d7311cbedc82
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2024-12-17 15:33:48 +0300

    ArmPkg: Refactored SVC handling.

--- ArmPkg/Library/ArmExceptionLib/ArmExceptionLib.c
+++ ArmPkg/Library/ArmExceptionLib/ArmExceptionLib.c
@@ -155,9 +155,6 @@ CommonCExceptionHandler (
   if ((UINTN)ExceptionType <= gMaxExceptionNumber) {
     if (gExceptionHandlers[ExceptionType]) {
       gExceptionHandlers[ExceptionType](ExceptionType, SystemContext);
-#if defined (MDE_CPU_AARCH64)
-      SystemContext.SystemContextAArch64->ESR = 0;
-#endif
       return EFI_SUCCESS;
     }
   } else {
