a5fc3d111f35a4738744031a7b2dce67a3ae0a78
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2025-02-27 14:06:29 +0300

    CI: Constructed separate workflows for common, arm, x86 packages and CodeQL.
    Signed-off-by: Savva Mitrofanov <savvamtr@gmail.com>

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -71,6 +71,7 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
                 "DynamicTablesPkg",
                 "EmbeddedPkg",
                 "EmulatorPkg",
+                "Ext4Pkg",
                 "IntelFsp2Pkg",
                 "IntelFsp2WrapperPkg",
                 "MdePkg",
@@ -210,6 +211,8 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
             "CryptoPkg/Library/MbedTlsLib/mbedtls", False))
         rs.append(RequiredSubmodule(
             "SecurityPkg/DeviceSecurity/SpdmLib/libspdm", False))
+        rs.append(RequiredSubmodule(
+            "OpenCorePkg", False))
         return rs
 
     def GetName(self):
