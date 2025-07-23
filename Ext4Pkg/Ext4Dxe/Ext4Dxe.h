2bbde997470adafbe14839620a2feb50340282d1
Pavel Naberezhnev <pavelnaberezhnev@gmail.com>
2025-07-23 18:48:27 +0300

    Added EFI_FILE_PROTOCOL Flush() dummy implementation
    The ext4 driver implements EFI_FILE_PROTOCOL_REVISION
    but was missing the required Flush() function. According to
    UEFI Specification v2.10, Section 13.5 (File Protocol),
    all protocol functions must be implemented.
    
    Changes:
    - Adds minimal Flush() function implementation
    
    Signed-off-by: Pavel Naberezhnev <pavelnaberezhnev@gmail.com>

--- Ext4Pkg/Ext4Dxe/Ext4Dxe.h
+++ Ext4Pkg/Ext4Dxe/Ext4Dxe.h
@@ -716,6 +716,27 @@ Ext4WriteFile (
   );
 
 /**
+  Flushes all modified data associated with a file to a device
+
+  @param[in]      This        A pointer to the EFI_FILE_PROTOCOL instance that is the file
+                              handle to flush data.
+
+  @retval EFI_SUCCESS          The data was flushed.
+  @retval EFI_NO_MEDIA         The device has no medium.
+  @retval EFI_DEVICE_ERROR     The device reported an error.
+  @retval EFI_VOLUME_CORRUPTED The file system structures are corrupted.
+  @retval EFI_WRITE_PROTECTED  The file or medium is write-protected.
+  @retval EFI_ACCESS_DENIED    The file was opened read only.
+  @retval EFI_VOLUME_FULL      The volume is full.
+
+**/
+EFI_STATUS
+EFIAPI
+Ext4Flush (
+  IN EFI_FILE_PROTOCOL  *This
+  );
+
+/**
   Returns a file's current position.
 
   @param[in]   This            A pointer to the EFI_FILE_PROTOCOL instance that
