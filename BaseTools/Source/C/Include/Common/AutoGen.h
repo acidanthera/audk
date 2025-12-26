ceef6c5fce23a3be0c52a86d1ba4a79e5175c897
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2025-12-26 14:53:16 +0300

    BaseTools/CommonLib: Consume MdePkg and MdeModulePkg
    BaseTools has been duplicating and adapting code that is defined in
    MdePkg and MdeModulePkg. This leads to desync issues where the same
    symbols may be backed by different functions with slightly different
    semantics and also fixes that apply only to BaseTools or only to MdePkg
    and MdeModulePkg.
    
    To address these issues, update BaseTools/Source/C to utilize the code
    from MdePkg and MdeModulePkg.
    
    Signed-off-by: Marvin Häuser <mhaeuser@posteo.de>

---
+++ BaseTools/Source/C/Include/Common/AutoGen.h
@@ -0,0 +1,93 @@
+/** @file
+  AutoGen definitions for edk2 package code consumption in BaseTools.
+
+  Copyright (c) 2023, Marvin Häuser. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _BT_AUTOGENH
+#define _BT_AUTOGENH
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <Base.h>
+#include <Library/PcdLib.h>
+
+extern GUID   gEfiCallerIdGuid;
+extern GUID   gEdkiiDscPlatformGuid;
+extern CHAR8  *gEfiCallerBaseName;
+
+#define EFI_CALLER_ID_GUID \
+  {0x00000000, 0x0000, 0x0000, {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}
+#define EDKII_DSC_PLATFORM_GUID \
+  {0x00000000, 0x0000, 0x0000, {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}
+
+// Definition of SkuId Array
+extern UINT64  _gPcd_SkuId_Array[];
+
+// Definition of PCDs used in this module
+
+#define _PCD_TOKEN_PcdMaximumAsciiStringLength          0U
+#define _PCD_SIZE_PcdMaximumAsciiStringLength           4
+#define _PCD_GET_MODE_SIZE_PcdMaximumAsciiStringLength  _PCD_SIZE_PcdMaximumAsciiStringLength
+#define _PCD_VALUE_PcdMaximumAsciiStringLength          0U
+#define _PCD_GET_MODE_32_PcdMaximumAsciiStringLength    _PCD_VALUE_PcdMaximumAsciiStringLength
+// #define _PCD_SET_MODE_32_PcdMaximumAsciiStringLength  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#define _PCD_TOKEN_PcdMaximumUnicodeStringLength          0U
+#define _PCD_SIZE_PcdMaximumUnicodeStringLength           4
+#define _PCD_GET_MODE_SIZE_PcdMaximumUnicodeStringLength  _PCD_SIZE_PcdMaximumUnicodeStringLength
+#define _PCD_VALUE_PcdMaximumUnicodeStringLength          0U
+#define _PCD_GET_MODE_32_PcdMaximumUnicodeStringLength    _PCD_VALUE_PcdMaximumUnicodeStringLength
+// #define _PCD_SET_MODE_32_PcdMaximumUnicodeStringLength  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#define _PCD_TOKEN_PcdMaximumLinkedListLength          0U
+#define _PCD_SIZE_PcdMaximumLinkedListLength           4
+#define _PCD_GET_MODE_SIZE_PcdMaximumLinkedListLength  _PCD_SIZE_PcdMaximumLinkedListLength
+#define _PCD_VALUE_PcdMaximumLinkedListLength          0U
+#define _PCD_GET_MODE_32_PcdMaximumLinkedListLength    _PCD_VALUE_PcdMaximumLinkedListLength
+// #define _PCD_SET_MODE_32_PcdMaximumLinkedListLength  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#define _PCD_TOKEN_PcdVerifyNodeInList          0U
+#define _PCD_SIZE_PcdVerifyNodeInList           1
+#define _PCD_GET_MODE_SIZE_PcdVerifyNodeInList  _PCD_SIZE_PcdVerifyNodeInList
+#define _PCD_VALUE_PcdVerifyNodeInList          1U
+#define _PCD_GET_MODE_BOOL_PcdVerifyNodeInList  _PCD_VALUE_PcdVerifyNodeInList
+// #define _PCD_SET_MODE_BOOL_PcdVerifyNodeInList  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#define _PCD_TOKEN_PcdFixedDebugPrintErrorLevel          0U
+#define _PCD_SIZE_PcdFixedDebugPrintErrorLevel           4
+#define _PCD_GET_MODE_SIZE_PcdFixedDebugPrintErrorLevel  _PCD_SIZE_PcdFixedDebugPrintErrorLevel
+#define _PCD_VALUE_PcdFixedDebugPrintErrorLevel          0xFFFFFFFFU
+#define _PCD_GET_MODE_32_PcdFixedDebugPrintErrorLevel    _PCD_VALUE_PcdFixedDebugPrintErrorLevel
+// #define _PCD_SET_MODE_32_PcdFixedDebugPrintErrorLevel  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#define _PCD_TOKEN_PcdDebugPropertyMask          0U
+#define _PCD_SIZE_PcdDebugPropertyMask           1
+#define _PCD_GET_MODE_SIZE_PcdDebugPropertyMask  _PCD_SIZE_PcdDebugPropertyMask
+#define _PCD_VALUE_PcdDebugPropertyMask          0xFFU
+#define _PCD_GET_MODE_8_PcdDebugPropertyMask     _PCD_VALUE_PcdDebugPropertyMask
+// #define _PCD_SET_MODE_8_PcdDebugPropertyMask  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#define _PCD_TOKEN_PcdDebugClearMemoryValue          0U
+#define _PCD_SIZE_PcdDebugClearMemoryValue           1
+#define _PCD_GET_MODE_SIZE_PcdDebugClearMemoryValue  _PCD_SIZE_PcdDebugClearMemoryValue
+#define _PCD_VALUE_PcdDebugClearMemoryValue          0xAFU
+#define _PCD_GET_MODE_8_PcdDebugClearMemoryValue     _PCD_VALUE_PcdDebugClearMemoryValue
+// #define _PCD_SET_MODE_8_PcdDebugClearMemoryValue  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#define _PCD_TOKEN_PcdDebugPrintErrorLevel          0U
+#define _PCD_SIZE_PcdDebugPrintErrorLevel           4
+#define _PCD_GET_MODE_SIZE_PcdDebugPrintErrorLevel  _PCD_SIZE_PcdDebugPrintErrorLevel
+#define _PCD_VALUE_PcdDebugPrintErrorLevel          0x8000004F
+#define _PCD_GET_MODE_32_PcdDebugPrintErrorLevel    _PCD_VALUE_PcdDebugPrintErrorLevel
+// #define _PCD_SET_MODE_32_PcdDebugPrintErrorLevel  ASSERT(FALSE)  // It is not allowed to set value for a FIXED_AT_BUILD PCD
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
